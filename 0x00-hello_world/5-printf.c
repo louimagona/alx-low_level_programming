c840c28ee036fc1a2074bfe3f097a06e44f0ed69
louimagona <louimagona2@gmail.com>
2021-06-17 22:24:48 +0300

    printf 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+/*Printing using the printf function*/
 int main(void)
 {
-	/*Printing a string to standard output*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
