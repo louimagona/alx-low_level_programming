a110fa53ee3caf65521312d637ebb67304af09ee
louimagona <louimagona2@gmail.com>
2021-06-22 17:14:51 +0300

    alphabet in small letters 5

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * print alphabet in small letters.
+ * print_alphabet - function to print abc
  *
- * Return: Always 0.
+ * Return: 0
  */
 void print_alphabet(void)
 {
