40dc5e5f389f11dcad78c7eae84bf9fae8ce7684
louimagona <louimagona2@gmail.com>
2021-06-22 19:54:57 +0300

    If character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _islower - checks if a character is lowercase
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int i;
+	int j = tolower(c).
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == j)
+		{
+			_putchar(31);
+		}
+		else
+		{
+			putchaf(30);
+		}
+
+	}
+	return (0);
+}
