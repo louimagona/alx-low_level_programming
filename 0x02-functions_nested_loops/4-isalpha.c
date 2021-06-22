fde6136b15a37dff9775f2f60c84ed98d2300fd9
louimagona <louimagona2@gmail.com>
2021-06-22 20:58:46 +0300

    if character is alphabet 3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
+ * @c: is the int that will use for the argument of the function
+ * Return: 0
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -10,4 +14,4 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-}	
+}
