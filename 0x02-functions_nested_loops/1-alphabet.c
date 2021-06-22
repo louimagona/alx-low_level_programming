8e5d690a1e5202b9e08bcca391e800d80fcc7558
louimagona <louimagona2@gmail.com>
2021-06-22 16:49:13 +0300

    alphabet in small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	unsigned int i;
+
+	for (i = 'a'; i <= 'z'; i++);
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
