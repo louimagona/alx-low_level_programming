e7c7ccaaa133c241f50db4a7265e75b52929a080
louimagona <louimagona2@gmail.com>
2021-06-23 22:37:29 +0300

    print from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_to_98 - print values from n to 98.
+ *@n: integer to be supplied by the user.
+ *@i: the iterator integer that scans the list.
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int i;
+	
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i < 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+
+			if (i > 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
+}
