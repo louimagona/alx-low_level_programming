12697cd9cb4dfefa901f6da4b505306467d08275
louimagona <louimagona2@gmail.com>
2021-06-23 17:51:56 +0300

    function that adds two integers1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * add -adds two integers and returns the result
- *@a set as the first parameters
- *@b set as the second parameter
- * Return: Always 0.
+ * add -adds two integers and returns the result.
+ *@a: set as the first parameters.
+ *@b: set as the second parameter.
+ * Return: the sum of the two integers.
  */
 int add(int a, int b)
 {
