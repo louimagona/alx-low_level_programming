8e2c94adbfe3af3aba37aeabf6dd26df38103d77
louimagona <louimagona2@gmail.com>
2021-06-25 00:12:09 +0300

    check if c is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 
 /**
- * _isdigit - check if c is a digit between 0 and 9;
- * @n: the integer to be supplied by user.
- * Return: Always 0.
- */
-                                                int _isdigit(int c)
+*_isdigit - check if c is a digit between 0 and 9.
+*@n: the integer to be supplied by user.
+*Return: Always 0.
+*/
+
+int _isdigit(int c)
 {
 	if ((c >= 48 && c <= 57) || (c >= 0 && c <= 9))
 	{
