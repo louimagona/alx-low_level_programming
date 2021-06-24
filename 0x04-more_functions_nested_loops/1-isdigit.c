7595da5841450a8b5be7d17d2747b100518b046c
louimagona <louimagona2@gmail.com>
2021-06-25 00:01:47 +0300

    check if c is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
- * _isdigit - check the code for ALX School students.
+ * _isdigit - check if c is a digit.
  * @n: the integer to be supplied by user.
  * Return: Always 0.
  */
-                                                int _isdigit(int c)                             {
-        if ((c >= 0 && c <= 9) || (c >= 48 && c <= 57))
-        {
-                return (1);                             }
-        else
-        {
-                return (0);
-        }
+                                                int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
