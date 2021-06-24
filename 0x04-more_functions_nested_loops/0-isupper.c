8c4be091d3987053be3df76de5870c4b95a7cc7e
louimagona <louimagona2@gmail.com>
2021-06-24 22:58:38 +0300

    determine if c is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks whether a character is uppercase.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if ((c + '0') == i)
+		{
+			return (1).
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
