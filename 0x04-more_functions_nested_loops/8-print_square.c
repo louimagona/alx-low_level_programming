7c1373a79b3a8a0cd5f8861405c74253bd44eaec
louimagona <louimagona2@gmail.com>
2021-06-25 16:23:01 +0300

    print square on terminal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints a square on terminal.
+ * @size: value to be supplied
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	char hash = '#';
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (size <= 0)
+			{
+				_putchar('\n');
+			}
+			_putchar(hash);
+		}
+	}
+	_putchar('\n');
+
+
+}
