b203e0e54a23d030db72d8bd6776121656d35b24
louimagona <louimagona2@gmail.com>
2021-06-25 14:17:36 +0300

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	
+	i = 0;
+	while (i < 10)
+	{
+		while(j < 15)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
