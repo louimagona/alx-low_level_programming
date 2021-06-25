244b144c35de51f0c959911c192d2218e70bf8c8
louimagona <louimagona2@gmail.com>
2021-06-25 10:47:06 +0300

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - check the code for ALX School students.
+ *
+ * Return: nothing.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+}
