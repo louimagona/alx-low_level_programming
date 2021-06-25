101b2a6553d0b772f6a0cbc70b84533f778bcd3d
louimagona <louimagona2@gmail.com>
2021-06-25 07:49:22 +0300

    multiply two parameter supplied

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* mul - func name.
+* @a: first parameter.
+* @b: second parameter.
+* Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
