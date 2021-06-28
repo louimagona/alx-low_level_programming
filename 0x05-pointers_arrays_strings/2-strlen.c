fe0eba451298802107847a52b81131f958c204ed
louimagona <louimagona2@gmail.com>
2021-06-28 16:48:24 +0300

    print length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strlen - prints the length of char pointer.
+ *@s: pointer to character array.
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	sizeof(s);
+}
