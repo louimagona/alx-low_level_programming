aa24197dfa4a6179d77d0604e302f34d1bcc98d8
louimagona <louimagona2@gmail.com>
2021-07-01 13:27:29 +0300

    concatenate with bound n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,23 @@
 #include "holberton.h"
-
 /**
- * _strncat - concatenate strings bounded.
- *@dest: pointer to string dest
- *@str: pointer to string str
- *@n : integer bound.
+ * _strncat - function that concatenates two strings
+ * @dest: parmater for dest to append to
+ * @src: parameter for src
+ * @n: parameter for n
  * Return: Always 0.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0';)
+	while (dest[i] != '\0')
+		a++;
+	while (src[j] != '\0' && j < n)
 	{
-		i++;
-	}
-
-	while (str[j] != '\0' && j < n)
-	{
-		dest[i] = str[j];
-
-		j++;
-		i++;
+		dest[i] = src[j];
+		b++;
+		a++;
 	}
 	return (dest);
 }
