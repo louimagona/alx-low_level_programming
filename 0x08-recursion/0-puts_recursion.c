8c664cde985453bb48fc2aa160c0cc452f376d2c
louimagona <louimagona2@gmail.com>
2021-07-07 11:32:25 +0300

    prints string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string on terminal
+ *@s: string supplied as parameter
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+recurssions in c.
