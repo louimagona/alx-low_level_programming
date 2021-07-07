8af04c07a94b2553f38e93ff7a1c29508bc07491
louimagona <louimagona2@gmail.com>
2021-07-07 16:28:16 +0300

    compute power using recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - computes power given two ints
  * @x: base integer
  * @y: power integer
- * Return: x power y 
+ * Return: x power y
  */
 
 int _pow_recursion(int x, int y)
@@ -13,5 +13,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y < 0)
 		return (-1);
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
