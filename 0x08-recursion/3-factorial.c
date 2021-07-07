808f4298fa26a8f8a08ad16f6fed6140b0ea8d81
louimagona <louimagona2@gmail.com>
2021-07-07 13:49:55 +0300

    compute factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * factorial - computer factorial of a number.
+ * @n: integer to obtain its factorial
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return (n * factorial(n - 1);
+}
