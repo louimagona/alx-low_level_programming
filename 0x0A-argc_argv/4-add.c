a311f651e1c0d00061c22e75d5c0697b97a0d6f6
louimagona <louimagona2@gmail.com>
2021-07-12 07:24:57 -0700

    func to add poitive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,11 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]) || atoi(argv[i]) < 0)
+		if (atoi(argv[i]) < 0)
+		{
+			continue;
+		}
+		if (!atoi(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
