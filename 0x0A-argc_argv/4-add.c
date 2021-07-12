e153f578cf55b0a866b0dd8f8ce0319828058379
louimagona <louimagona2@gmail.com>
2021-07-12 06:59:10 -0700

    fun to add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: count number of cml arguments
+ * @argv: array size of the arguments
+ * Return: 0 as exit success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
