35f148bd635d2de7222ea33bf9bef26996b60730
louimagona <louimagona2@gmail.com>
2021-07-12 13:52:44 +0300

    func that print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,11 @@
  * main - print its name
  * @argc: count number of cml arguments
  * @argv: array size of the arguments
+ * Return: 0 as exit success
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
+	return (0);
 }
