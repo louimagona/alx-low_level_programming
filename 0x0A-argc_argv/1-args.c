fcf390c80c34820149b57bb2ef8d9bbbe869c983
louimagona <louimagona2@gmail.com>
2021-07-12 04:37:36 -0700

    func to print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: count number of cml arguments
+ * @argv: array size of the arguments
+ * Return: 0 as exit success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+        int count, args;
+
+	args = 0;
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			args += 1;
+		}
+	}
+	printf("%d\n", args);
+        return (0);
+}
