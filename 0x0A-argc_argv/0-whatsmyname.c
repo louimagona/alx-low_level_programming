36c67d92987d57b7730df74d93844fd30e618192
louimagona <louimagona2@gmail.com>
2021-07-12 13:30:49 +0300

    func that print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: count number of cml arguments
+ * @argv: array size of the arguments
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
