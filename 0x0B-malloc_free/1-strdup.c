07b882328966c3c195601f0c83e1bd276480232c
louimagona <louimagona2@gmail.com>
2021-07-13 09:47:36 -0700

    string duplicate using malloc 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,12 @@ char *_strdup(char *str)
 	int len = 0;
 
 	while (str[len])
-	{
 		len++;
-	}
 	cpy = malloc(len + 1);
 	ptr = cpy;
 	while (*str)
-	{
-		*ptr++ = *str;
-	}
+		*ptr++ = *str++
 	*ptr = '\0';
 	return (ptr);
 	free(ptr);
 }
-
