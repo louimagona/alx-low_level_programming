43a8758e9c6547759c14c241c844f9149f76a2bb
louimagona <louimagona2@gmail.com>
2021-07-13 12:09:49 -0700

    duplicate of a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,19 +6,18 @@
  * @str: string to be supplied
  * Return: pointer to newly allocated memory
  */
-
 char *_strdup(char *str)
 {
-    char *src;
-    char *p;
-    int len = 0;
+	char *src;
+	char *p;
+	int len = 0;
 
-    while (str[len])
-        len++;
-    src = malloc(len + 1);
-    p = src;
-    while (*str)
-        *p++ = *str++;
-    *p = '\0';
-    return (str);
+	while (str[len])
+		len++;
+	src = malloc(len + 1);
+	p = src;
+	while (*str)
+		*p++ = *str++;
+	*p = '\0';
+	return (p);
 }
