17cd7b799bcf76a69e5977d13be87e1fb49030c7
louimagona <louimagona2@gmail.com>
2021-07-14 17:31:56 +0300

    string duplicate with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,17 +8,26 @@
  */
 char *_strdup(char *str)
 {
-	char *src;
 	char *p;
-	int len = 0;
+	int len, i;
+	len = 0;
 
-	while (str[len] != '\0')
+	if (str == NULL)
+		return (NULL);
+6
+	while (str[len])
 		len++;
-	src = malloc((len + 1));
-	p = src;
-	while (*str)
-		*p++ = *str++;
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		p[i] = str[i];
+		i++;
+	}
 	*p = '\0';
 	return (p);
 	free(p);
+
 }
