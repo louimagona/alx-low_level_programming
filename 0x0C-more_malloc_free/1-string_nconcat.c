a30a9a5744a1b76b0742c1e4ea7e050ad96112ab
louimagona <louimagona2@gmail.com>
2021-07-15 22:13:36 +0300

    concatenate strings upto n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,49 @@
-#include "holberton.h"
+include "holberton.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatetenate two strings upto n
- * @s1: string 1
- * @s2: string 2
- * @n: limit of concatenation
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, k, l, m;
-	char *ptr;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-	i = 0;
-	while (s1[i])
-		i++;
-	ptr = malloc(sizeof(char) * (i + n + 1));
-	if (ptr == NULL)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	k = 0;
-	while (k < i)
-	{
-		ptr[k] = s1[k];
-		k++;
-	}
-	l = i;
-	m = 0;
-	while (m < n)
-	{
-		ptr[l] = s2[m];
-		l++;
-		m++;
-	}
-	return (ptr);
 
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
 
+	sout[i] = '\0';
+
+	return (sout);
 }
