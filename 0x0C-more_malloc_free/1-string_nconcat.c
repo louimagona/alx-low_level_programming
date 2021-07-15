233be50f411fd7d4241b436b57b1661f74d7dea9
louimagona <louimagona2@gmail.com>
2021-07-15 21:45:17 +0300

    concatenate strings upto n

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatetenate two strings upto n
+ * @s1: string 1
+ * @s2: string 2
+ * @n: limit of concatenation
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, l, m;
+	char *ptr;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	ptr = malloc(sizeof(char) * (i + n + 1));
+	if (ptr == NULL)
+		return (NULL);
+	k = 0;
+	while (k < i)
+	{
+		ptr[k] == s1[k];
+		k++;
+	}
+	l = i;
+	m = 0;
+	while (m < n)
+	{
+		p[l] = s2[m];
+		l++;
+		m++;
+	}
+	return (ptr);
+
+
+}
