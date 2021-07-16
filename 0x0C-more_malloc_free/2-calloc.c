6d1751828eaf748c56cdf5bb8904c187bf747f94
louimagona <louimagona2@gmail.com>
2021-07-16 20:08:14 +0300

    allocate wit malloc immitating calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
+	usigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 	return (ptr);
 }
