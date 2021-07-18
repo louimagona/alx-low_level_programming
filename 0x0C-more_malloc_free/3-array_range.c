a82f7864b42df6a33481aba8c92d6a10c3afb2ae
louimagona <louimagona2@gmail.com>
2021-07-18 14:20:53 +0300

    allocate memory for an incoming array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,33 +1,29 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
- * array_range - prints range from min to max
- * @min: the starting point
- * @max: the end point of the range
- * Return: pointer to the allocated memory location
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
  */
-
 int *array_range(int min, int max)
 {
+	int *ar;
 	int i;
-
-	unsigned int len;
-
-	int *ptr;
 
 	if (min > max)
 		return (NULL);
-	len = (max - min) + 1;
-	ptr = malloc(len * sizeof(int));
-	if (ptr == NULL)
+
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
+	if (ar == NULL)
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		ptr[i] = min;
-		i++;
-		min++;
-	}
-	return (ptr);
+
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
+
+	return (ar);
 }
