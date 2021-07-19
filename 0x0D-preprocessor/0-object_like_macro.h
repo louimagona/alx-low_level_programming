3e1131ff952b1383f28e7207e18c0398c30fb97a
louimagona <louimagona2@gmail.com>
2021-07-19 17:47:33 +0300

    print file name

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef SIZE_H
-#define SIZE_H
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
+
 #define SIZE 1024
 
 #endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name 
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
