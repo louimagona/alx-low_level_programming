1fcf02e970759c26eb21202525fdde4e6fa19b97
louimagona <louimagona2@gmail.com>
2021-07-20 16:32:57 +0300

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		(*p).age = age;
 		(*p).owner = owner;
 	}
-	else
+	else if (p == NULL || !(name) || !(owner))
 	{
 		free(p);
 		return (NULL);
