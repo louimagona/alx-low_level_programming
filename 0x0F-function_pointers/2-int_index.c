8af523e2203d25fa265f3f42767067418a99a08f
louimagona <louimagona2@gmail.com>
2021-07-21 12:04:32 -0700

    added a test for array and cmp availability

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,6 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (array && cmp)
+	{
 	for (i = 0; i < size; i++)
 	{
 		cmp(array[i]);
@@ -24,6 +26,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			return (-1);
 		}
+	}
 	}
 	return (i);
 }
