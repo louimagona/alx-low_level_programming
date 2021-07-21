a5934a63f8e7f2777bd120a5a5b84ce95cc0abdc
louimagona <louimagona2@gmail.com>
2021-07-21 05:46:04 -0700

    func that execute a func given as parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
-	i = 0;
-	while (i < size)
+	if (array && size && action)
 	{
+		for (i = 0; i < size; i++)
 		action(array[i]);
-		i++;
 	}
 }
