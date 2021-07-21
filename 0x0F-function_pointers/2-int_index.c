a787aa5e8f2e7d34953df4018e9341055827169d
louimagona <louimagona2@gmail.com>
2021-07-21 09:15:33 -0700

    func that serches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - serche for a matching integer from array
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ * Return: index of the first match
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]))
+		{
+			break;
+		}
+	
+	}
+	return (i);
+}
