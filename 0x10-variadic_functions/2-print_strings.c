7a6e1ee7c2c974ddda86b14354dab786c7604f3e
louimagona <louimagona2@gmail.com>
2021-07-23 01:24:14 -0700

    test if pointer is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
 
 /**
  * print_strings - print string
- * @separator: separating string 
+ * @separator: separating string
  * @n: limit of characters
  * Return: nothing
  */
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (!va_arg(p, char *))
+		if (va_arg(p, char *) == NULL)
 			printf("(nil)");
 		if (!separator)
 			printf("%s", va_arg(p, char *));
