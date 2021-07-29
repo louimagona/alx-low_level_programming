dc69f3abdce5104cc83b8a508ccba65699c05d5c
louimagona <louimagona2@gmail.com>
2021-07-29 18:38:34 +0300

    print members of each node of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
-/**
  * print_list - prints all the members of the data structure
  * @h: pointer to a struct data type
  * Return: number of nodes
@@ -32,7 +15,8 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n",0, "(nil)");
-		printf("[%d] %s\n",h->len, h->str);
+		else
+			printf("[%d] %s\n",h->len, h->str);
 		h = h->next;
 		i++;
 	}
