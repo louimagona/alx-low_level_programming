e1d1f2cf8da77f19e3209e1bd733cb427b231af1
louimagona <louimagona2@gmail.com>
2021-08-03 00:41:52 +0300

    created a func that adds a node at end of list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,23 +1,35 @@
 #include "lists.h"
 
 /**
- * add_node - adds a node at beginning of list
+ * add_nodeint_end - adds a node at beginning of list
  * @head: head pointer
  * @n: some value added I dont see for what
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-        listint_t *newnode;
+        listint_t *newnode, *temp;
 
         newnode = (listint_t *)malloc(sizeof(listint_t));
         if (newnode == NULL)
                 return (NULL);
-        else
-        {
-        newnode->n = n;
-        newnode->next = *head;
-        *head = newnode;
-        }
+
+	temp = *head;
+	newnode->n = n;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = newnode;
+	}
+
         return (*head);
 }
