a3abdd5cb9503c8f4810631803b872f4e0ed9d36
louimagona <louimagona2@gmail.com>
2021-08-03 15:38:53 +0300

    created a func to free listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the linked list
+ * @head: head pointer
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *traverse;
+
+	traverse = head;
+
+	while (traverse->next != NULL)
+	{
+		free(traverse);
+		traverse = traverse->next;
+	}
+	free(head);
+}
