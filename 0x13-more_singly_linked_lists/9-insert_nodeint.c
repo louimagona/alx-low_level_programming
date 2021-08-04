bdaea371046e5f8b73508883ab92aec247372bcb
louimagona <louimagona2@gmail.com>
2021-08-04 07:34:16 -0700

    created a func that inserts a new node at an index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at an nth index
+ * @head: head pointer
+ * @n: int to be inserted as data in the new node
+ * Return: pointer to the newnode
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newnode, *t;
+	unsigned int count;
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+	if (!newnode)
+		return (NULL);
+	newnode->n = n;
+	t = *head;
+	count = 0;
+	while (t)
+	{
+		if (count == idx)
+		{
+			newnode->next = t->next;
+			t->next = newnode;
+		}
+		t = t->next;
+		count++;
+	}
+	return (newnode);
+
+}
